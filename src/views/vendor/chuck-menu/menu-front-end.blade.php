f5473ec81165a00bf08e1b95d3f1ed4b89dd2e53
KarelBrijs <karel@chuck.be>
2020-01-13 13:07:11 +0100

    update composer for L6+

--- src/views/vendor/chuck-menu/menu-front-end.blade.php
+++ src/views/vendor/chuck-menu/menu-front-end.blade.php
@@ -1,59 +1,30 @@
 @if(isset($menus))
-<ul class="nav navbar-nav">
+<ul class="navbar-nav ml-auto">
     @foreach($menus as $m)
         @if($m->depth == 0)
-            <li
-                @foreach($menus->where('depth', 1)->where('parent', $m->id) as $subm)
-                    @if($loop->first)
-                        class="dropdown"
-                    @endif
-                @endforeach
-            >
-                <a href="{{ $m->link }}" 
-                    @foreach($menus->where('depth', 1)->where('parent', $m->id) as $subm)
-                        @if($loop->first)
-                            class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"
-                        @endif
-                    @endforeach
-                >
-                    {{ $m->label }}
-                    @foreach($menus->where('depth', 1)->where('parent', $m->id) as $subm)
-                        @if($loop->first)
-                            <span class="caret"></span>
-                        @endif
-                    @endforeach
+            <li class="nav-item{{ $menus->where('depth', 1)->where('parent', $m->id)->count() > 0 ? ' dropdown' : '' }}">
+                <a href="{{ $m->link }}" class="nav-link{{ $menus->where('depth', 1)->where('parent', $m->id)->count() > 0 ? ' dropdown-toggle' : '' }}" {{ $menus->where('depth', 1)->where('parent', $m->id)->count() > 0 ? 'data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"' : '' }}>
+                    {{ $m->label }} {!! $menus->where('depth', 1)->where('parent', $m->id)->count() > 0 ? '<span class="caret"></span>' : '' !!}
                 </a>
                 @foreach($menus->where('depth', 1)->where('parent', $m->id) as $subm)
-                        @if($loop->first)
+                    @if($loop->first)
+                    <ul class="dropdown-menu">
+                    @endif
+                        <li class="{{ $menus->where('depth', 2)->where('parent', $subm->id)->count() > 0 ? 'dropdown dropdown-submenu' : '' }}">
+                            <a href="{{ $subm->link }}" class="{{ $menus->where('depth', 2)->where('parent', $subm->id)->count() > 0 ? 'dropdown-toggle' : '' }}" {{ $menus->where('depth', 2)->where('parent', $subm->id)->count() > 0 ? 'data-toggle="dropdown"' : '' }}>{{ $subm->label }}</a>
+                            @foreach($menus->where('depth', 2)->where('parent', $subm->id) as $subsubm)
+                            @if($loop->first)
                             <ul class="dropdown-menu">
-                        @endif
-                                <li 
-                                    @foreach($menus->where('depth', 2)->where('parent', $subm->id) as $subsubm)
-                                        @if($loop->first)
-                                            class="dropdown dropdown-submenu"
-                                        @endif
-                                    @endforeach
-                                >
-                                    <a href="{{ $subm->link }}"
-                                        @foreach($menus->where('depth', 2)->where('parent', $subm->id) as $subsubm)
-                                            @if($loop->first)
-                                                class="dropdown-toggle" data-toggle="dropdown"
-                                            @endif
-                                        @endforeach
-                                    >{{ $subm->label }}</a>
-                                    @foreach($menus->where('depth', 2)->where('parent', $subm->id) as $subsubm)
-                                        @if($loop->first)
-                                            <ul class="dropdown-menu">
-                                        @endif
-                                            <li><a href="{{ $subsubm->link }}">{{ $subsubm->label }}</a></li>
-                                        @if($loop->last)
-                                            </ul>
-                                        @endif
-                                    @endforeach
-                                </li>
-                        @if($loop->last)
+                            @endif
+                                <li><a href="{{ $subsubm->link }}">{{ $subsubm->label }}</a></li>
+                            @if($loop->last)
                             </ul>
-                        @endif
+                            @endif
+                            @endforeach
+                        </li>
+                    @if($loop->last)
+                    </ul>
+                    @endif
                 @endforeach
             </li>
         @endif
